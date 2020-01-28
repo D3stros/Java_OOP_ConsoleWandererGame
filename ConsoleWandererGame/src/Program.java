b8499a5c2c003619e04f918284df01768efc272f
D3stros <patrick.fallmann@hotmail.com>
2020-01-28 19:57:47 +0100

    Implementing controls and user feedback for the ConsoleWanderer game

--- ConsoleWandererGame/src/Program.java
+++ ConsoleWandererGame/src/Program.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Program {
 
@@ -8,12 +9,22 @@ public class Program {
 		Player player1 = new Player(fieldGame);
 		fieldGame.printField();
 		
-		player1.rotateLeft(fieldGame);
-		player1.goForward(fieldGame);
-		player1.goForward(fieldGame);
-		player1.goForward(fieldGame);
-
-
+		String decision;
+		int temp = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(temp == 0) {
+			System.out.println("Please do something (g= go forward, l = left turn, r = right turn, q = quit program");
+			decision = sc.next();
+			
+			switch(decision) {
+			case "g": player1.goForward(fieldGame);break;
+			case "l": player1.rotateLeft(fieldGame);break;
+			case "r": player1.rotateRight(fieldGame);break;
+			case"q": temp = 1; System.out.println("The program was stopped");break;
+			default: System.out.println("You entered a wrong input");break;
+			}
+		}
 	}
-
 }
